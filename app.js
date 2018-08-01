6ad3a4e16d4ec6774987220ca77f52002dbd3ad2
Makhabbat Kuzhaniyazova <makonya2302@gmail.com>
2018-08-01 15:37:26 +0600

    Variables

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-// Log to console
-console.log('Hello World!');
-console.log(123);
-console.log(true);
-var greeting = 'Hello';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
+// //var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// //Init var
+// var greeteng;
+// console.log(greeteng);
+// greeteng = 'Hello';
+// console.log(greeteng);
+
+// //letters,numbers, _, $
+// //Can not start with number
+
+// //Multi word vars
+// var firstName = 'John'; // Camelcase
+// var first_Name = 'Sara'; // Underscore case
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+// //LET
+// let name2 = 'John Doe';
+// console.log(name2);
+// name2 = 'Steve Smith';
+// console.log(name2);
 
 
-console.error('This is some error');
-//console.clear();
-console.warn('This is a warning');
-console.time('Hello');
-  console.log('Hello World!');
-  console.log('Hello World!');
-  console.log('Hello World!');
-  console.log('Hello World!');
-  console.log('Hello World!');
-console.timeEnd('Hello');
-console.table({a:1, b:2});
+
+// // CONST
+// const name = 'John';
+// console.log(name);
+// // Can not reassign
+// name = 'Sara';
+// // Have to assign a value
+// const greeteng;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
