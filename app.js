816832fafb9ead9adfa5926b93703b4284e8d976
Makhabbat Kuzhaniyazova <makonya2302@gmail.com>
2018-08-01 15:56:17 +0600

    Data Types

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
-// //var, let, const
+//PRIMITIVE
 
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// String
+const name = 'john Doe';
+// Number
+const age = 45;
+//Boolean
+const hasKids = true;
+//Null
+const car = null;
+//Undefined
+let test;
+//Symbol
+const sym = Symbol();
 
-// //Init var
-// var greeteng;
-// console.log(greeteng);
-// greeteng = 'Hello';
-// console.log(greeteng);
-
-// //letters,numbers, _, $
-// //Can not start with number
-
-// //Multi word vars
-// var firstName = 'John'; // Camelcase
-// var first_Name = 'Sara'; // Underscore case
-// var FirstName = 'Tom'; // Pascal case
-// var firstname;
-
-// //LET
-// let name2 = 'John Doe';
-// console.log(name2);
-// name2 = 'Steve Smith';
-// console.log(name2);
-
-
-
-// // CONST
-// const name = 'John';
-// console.log(name);
-// // Can not reassign
-// name = 'Sara';
-// // Have to assign a value
-// const greeteng;
-
-const person = {
-  name: 'John',
-  age: 30
+//REFERENCE TYPES = Objects
+//Array
+const hobbies = ['movies', 'music'];
+//Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
 }
+const today = new Date();
 
-person.name = 'Sara';
-person.age = 32;
-
-console.log(person);
-
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-
-console.log(numbers);
+console.log(today);
+console.log(typeof today);
