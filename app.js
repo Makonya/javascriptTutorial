2a016087dbc3a16c888ac6f8b8c8cda74b5c420a
Makhabbat Kuzhaniyazova <makonya2302@gmail.com>
2018-08-05 00:19:45 +0600

    String Methods

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
-const num1 = 100;
-const num2 = 50;
-let vat;
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello my name is Brad';
+const tags = 'web design,web development,programming';
 
-//Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+let val;
 
-//Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2,33,4,1,55,6,3,-2);
-val = Math.max(2,33,4,1,55,6,3,-2);
-val = Math.floor(Math.random() * 20 + 1);
+val = firstName + lastName;
+
+//Concatenation
+val = firstName + ' ' + lastName;
+
+//Append
+val = 'Brad ';
+val += 'Travarsy';
+
+val = 'Hello, my name is ' + firstName + 'and I am' + age;
+
+//Escaping
+val = 'That\'s awesome, I can\'t wait';
+
+//Length
+val = firstName.length;
+
+//concat
+val = firstName.concat(' ', lastName);
+
+//Change case
+val = firstName.toLocaleUpperCase();
+val = firstName.toLocaleLowerCase();
+
+val = firstName[2];
+
+//indexOf()
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+//charAt()
+val = firstName.charAt('2');
+//Get last char
+val = firstName.charAt(firstName.length-1);
+
+//substring()
+val = firstName.substring(0, 4);
+
+//slice()
+val = firstName.slice(0,4);
+val = firstName.slice(-3);
+
+//split()
+val = str.split(' ');
+val = tags.split(',');
+
+//replace()
+val = str.replace('Brad', 'Jack');
+
+//includes()
+val = str.includes('Hello');
 
 console.log(val);
