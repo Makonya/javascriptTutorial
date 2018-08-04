004fb077fdfb003b26b8befb230dd2d4b2a773bb
Makhabbat Kuzhaniyazova <makonya2302@gmail.com>
2018-08-05 00:38:52 +0600

    Template Literals

--- app.js
+++ app.js
@@ -1,61 +1,35 @@
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 36;
-const str = 'Hello my name is Brad';
-const tags = 'web design,web development,programming';
+const name = 'John';
+const age = 30;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
 
-let val;
+// Without template strings(es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
 
-val = firstName + lastName;
+html = '<ul>' +
+       '<li>Name: ' + name + '</li>' +
+       '<li>Age: ' + age + '</li>' +
+       '<li>Job: ' + job + '</li>' +
+       '<li>City: ' + city + '</li>' +
+       '</ul';
 
-//Concatenation
-val = firstName + ' ' + lastName;
+function hello(){
+  return 'hello';
+}
 
-//Append
-val = 'Brad ';
-val += 'Travarsy';
+//With template strings (es6)
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>  
+  </ul>
+`
 
-val = 'Hello, my name is ' + firstName + 'and I am' + age;
 
-//Escaping
-val = 'That\'s awesome, I can\'t wait';
-
-//Length
-val = firstName.length;
-
-//concat
-val = firstName.concat(' ', lastName);
-
-//Change case
-val = firstName.toLocaleUpperCase();
-val = firstName.toLocaleLowerCase();
-
-val = firstName[2];
-
-//indexOf()
-val = firstName.indexOf('l');
-val = firstName.lastIndexOf('l');
-
-//charAt()
-val = firstName.charAt('2');
-//Get last char
-val = firstName.charAt(firstName.length-1);
-
-//substring()
-val = firstName.substring(0, 4);
-
-//slice()
-val = firstName.slice(0,4);
-val = firstName.slice(-3);
-
-//split()
-val = str.split(' ');
-val = tags.split(',');
-
-//replace()
-val = str.replace('Brad', 'Jack');
-
-//includes()
-val = str.includes('Hello');
-
-console.log(val);
+document.body.innerHTML = html; 
